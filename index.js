bc7afd552fd9b4ef595a40025b3721d812a0fa04
Jose Luciano Guzman Santiago <deckblank@gmail.com>
2023-02-26 13:50:47 -0500

    FIX: to use with lambda

--- index.js
+++ index.js
@@ -2,21 +2,25 @@ import { APP_ENV, PAIS, PORT, PROYECTO } from "./config/enviroments.js";
 
 import { app } from "./app.js";
 import { db } from "./db/index.js";
-import http from "http";
 import { logger } from "./config/logger.js";
+
+//import http from "http";
+
 
 //import { sessMabUsers } from './db/mongodb/models';
 
-export const server = http.createServer(app);
+//export const server = http.createServer(app);
 
 const initServer = async (server) => {
   await db;
-  await server.listen(PORT);
+  //await server.listen(PORT);
   logger.info(`
     App: ${PROYECTO}
     Country: ${PAIS}
     Enviroment: ${APP_ENV}
     Port: ${PORT}
     Date: ${new Date()}`);
+  return app;
 };
-initServer(server);
+
+exports.handler = initServer(server);
